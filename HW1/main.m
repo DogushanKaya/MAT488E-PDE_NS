b5e53c096037c8a9b92a2a80c123e5ce8341d929
Dogushan Kaya <dogushankaya.itu@gmail.com>
2017-10-18 15:45:42 +0300

    Create main.m

---
+++ HW1/main.m
@@ -0,0 +1,17 @@
+
+error = 1e-7;
+r1 = 0.48;  T1final = 0.192;
+r2 = 0.52;  T2final = 0.208;
+deltaX = 0.1;
+Jvalues = 0:deltaX:1;
+deltaT1 = r1 * deltaX ^ 2;
+deltaT2 = r2 * deltaX ^ 2;
+T1values = 0:deltaT1:T1final;
+T2values = 0:deltaT2:T2final;
+
+
+while result > error
+   
+    
+    
+end
